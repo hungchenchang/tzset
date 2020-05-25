a1514ddb0674efcff638afa83a973c22ddb530d6
Steven Chang <steven_chang@everfocus.com.tw>
2020-05-25 15:20:04 +0800

    Add DateTimeConfigurationChangeNotification

--- tzset.cc
+++ tzset.cc
@@ -1,15 +1,23 @@
 #include <nan.h>
+#include <node.h>
+#include <v8.h>
+#include <time.h>
 
 using namespace v8;
+using v8::Date;
+using v8::Isolate;
+
 
 void tzset(const Nan::FunctionCallbackInfo<v8::Value>& args)
 {
   //printf("tzset is caled args = %d\n", args.Length());
+  Isolate *isolate = Isolate::GetCurrent();
 #ifdef _WIN32
   _tzset();
 #else
   tzset();
 #endif
+  Date::DateTimeConfigurationChangeNotification(isolate);
   int result = 0;
   v8::Local<v8::Number> num = Nan::New(result);
   args.GetReturnValue().Set(num);
